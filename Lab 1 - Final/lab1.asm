c4a15cb808b98b3da97e58d0641d661aefd8080c
Guilherme Kowalczuk <guilhermekowalczuk@gmail.com>
2021-09-09 14:56:54 +0000

    minor styling changes

--- Lab 1 - Final/lab1.asm
+++ Lab 1 - Final/lab1.asm
@@ -21,7 +21,7 @@
 
 ; Ponto de entrada para RESET.
      .ORG	0 
-            JMP  	RESET
+			JMP  	RESET
 
      .ORG	0x100
 RESET:
@@ -36,50 +36,51 @@ RESET:
 			LDI		R16,HIGH(RAMEND)
 			OUT		SPH,R16
 
-            CALL	USART_INIT					; Inicializa USART.
+			CALL 	USART_INIT					; Inicializa USART.
 
 FIRST_PROMPT:
 			LDI		ZH,HIGH(2*PROMPT_INICIAL)	; Imprime ...
-            LDI		ZL,LOW(2*PROMPT_INICIAL)
-			CALL	PRINT_STRING
+			LDI		ZL,LOW(2*PROMPT_INICIAL)
+			CALL 	PRINT_STRING
 
 			LDI		R16,'I'						; Opção inicial padrão é incremento
 			STS		CARACTERE,R16
 
 FOREVER:
-			CALL	USART_LAZY_RECEIVE			; R16 <- caractere recebido.
-			SBRS	R17,RXC0
+			CALL 	USART_LAZY_RECEIVE			; R16 <- caractere recebido.
+			SBRS 	R17,RXC0
 			JMP		WAIT_SWITCH
 			STS		CARACTERE,R16
 
 CHAR_PROMPT:
 			LDI		ZH,HIGH(2*PROMPT_ESCOLHA)	; Imprime ...
-            LDI		ZL,LOW(2*PROMPT_ESCOLHA)
-			CALL	PRINT_STRING
-			CALL	USART_TRANSMIT			; Imprime caractere recebido.
+			LDI		ZL,LOW(2*PROMPT_ESCOLHA)
+			CALL 	PRINT_STRING
+			CALL 	USART_TRANSMIT			; Imprime caractere recebido.
 			LDI		R16,RETURN
-			CALL	USART_TRANSMIT
+			CALL 	USART_TRANSMIT
 			LDI		R16,LINEFEED
-			CALL	USART_TRANSMIT
+			CALL 	USART_TRANSMIT
 
 WAIT_SWITCH:								; Escuta por ativação do SWITCH
 			IN		R16, PIND
-			ANDI	R16,0b10000000
-			BRNE	FOREVER
+			ANDI 	R16,0b10000000
+			BRNE 	FOREVER
 
 WAIT_SWITCH_RELEASE:						; Espera SWITCH desativar
 			IN		R16, PIND
-			ANDI	R16,0b10000000
-			BREQ	WAIT_SWITCH_RELEASE
+			ANDI 	R16,0b10000000
+			BREQ 	WAIT_SWITCH_RELEASE
+			
 			LDS		R18,CARACTERE
 			CPI		R18,'d'					; compara com o R18
-			BREQ	DECREMENTS				
+			BREQ 	DECREMENTS				
 			CPI		R18,'D'
-			BREQ	DECREMENTS				; if R18!=0, then we decrement...
+			BREQ 	DECREMENTS				; if R18!=0, then we decrement...
 INCREMENTS:									; else we increment
 			INC		R20
 			CPI		R20,16
-			BREQ	OVERFLOW
+			BREQ 	OVERFLOW
 			JMP		OUTPUT
 DECREMENTS:									; code for decrement
 			CPI		R20,0
@@ -113,15 +114,15 @@ PRINT_STRING:
 			PUSH	R16						; Salva o registrador R16.
 
 PRINT_STRING_LOOP:
-            LPM		R16,Z+
-            CPI		R16,'$'
-            BREQ	PRINT_STRING_END		; Se não chegou no caractere de término '$', usa USART.
-            CALL	USART_TRANSMIT
-            JMP		PRINT_STRING_LOOP
+			LPM		R16,Z+
+			CPI		R16,'$'
+			BREQ	PRINT_STRING_END		; Se não chegou no caractere de término '$', usa USART.
+			CALL	USART_TRANSMIT
+			JMP		PRINT_STRING_LOOP
 
 PRINT_STRING_END:
 			POP		R16						; Restaura R16 e retorna.
-            RET
+			RET
 				
 ;*************************************************************************************
 ;  PROTOCOLO USART
@@ -139,16 +140,16 @@ PRINT_STRING_END:
 
 USART_INIT:
 			LDI		R17,HIGH(BAUD_RATE)			; Estabelece Baud Rate.
-            STS		UBRR0H,R17
-            LDI		R16,LOW(BAUD_RATE)
-            STS		UBRR0L,R16
-            LDI		R16,(1<<RXEN0)|(1<<TXEN0)	; Habilita receptor e transmissor.
+			STS		UBRR0H,R17
+			LDI		R16,LOW(BAUD_RATE)
+			STS		UBRR0L,R16
+			LDI		R16,(1<<RXEN0)|(1<<TXEN0)	; Habilita receptor e transmissor.
 
-            STS		UCSR0B,R16
-            LDI		R16,(0<<USBS0)|(1<<UCSZ01)|(1<<UCSZ00)	; Frame: 8 bits dado, 1 stop bit,
-            STS		UCSR0C,R16								;        sem paridade.
+			STS		UCSR0B,R16
+			LDI		R16,(0<<USBS0)|(1<<UCSZ01)|(1<<UCSZ00)	; Frame: 8 bits dado, 1 stop bit,
+			STS		UCSR0C,R16								;        sem paridade.
 
-            RET
+			RET
 
 
 ;*******************************************
@@ -161,10 +162,10 @@ USART_TRANSMIT:
 			PUSH	R17					; Salva R17 na pilha.
 
 WAIT_TRANSMIT:
-            LDS		R17,UCSR0A
-            SBRS	R17,UDRE0			; Aguarda BUFFER do transmissor ficar vazio.		
-            RJMP	WAIT_TRANSMIT
-            STS		UDR0,R16			; Escreve dado de R16 no BUFFER.
+			LDS		R17,UCSR0A
+			SBRS	R17,UDRE0			; Aguarda BUFFER do transmissor ficar vazio.		
+			RJMP	WAIT_TRANSMIT
+			STS		UDR0,R16			; Escreve dado de R16 no BUFFER.
 			
 			POP		R17					; Restaura R17 da pilha e retorna.
 			RET
@@ -198,7 +199,7 @@ WAIT_RECEIVE:
 
 USART_LAZY_RECEIVE:
 			LDS		R17,UCSR0A
-			SBRC	R17,RXC0
+			SBRC 	R17,RXC0
 			LDS		R16,UDR0			; Le dado do BUFFER e escreve em R16.
 			RET
 
