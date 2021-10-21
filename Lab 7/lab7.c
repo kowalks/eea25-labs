859ebac791bd427a18eace7494de385cf710ef6b
Guilherme Kowalczuk <guilhermekowalczuk@gmail.com>
2021-10-21 08:50:57 +0000

    Lab7 shoud work

---
+++ Lab 7/lab7.c
@@ -0,0 +1,211 @@
+#include <avr/io.h>
+#include <stdio.h>
+#include <math.h>
+
+#define F_CPU 16000000UL
+
+// Function prototype
+void USART0Init(void);
+int USART0SendByte(char u8Data, FILE *stream);
+int USART0ReceiveByte(FILE *stream);
+
+void USART1Init(void);
+int USART1SendByte(char u8Data, FILE *stream);
+int USART1ReceiveByte(FILE *stream);
+
+void MasterModule(void);
+void SlaveModule(void);
+
+void initTempo(void);
+void resetServo(void);
+
+int isSign(char c) { return c == '+' || c == '-'; };
+int isDigit(char d) { return d >= '0' && d <= '9'; }
+
+/*  Stream for USART0  */
+FILE usart0_str = FDEV_SETUP_STREAM(USART0SendByte, USART0ReceiveByte, _FDEV_SETUP_RW);
+FILE usart1_str = FDEV_SETUP_STREAM(USART1SendByte, USART1ReceiveByte, _FDEV_SETUP_RW);
+
+int main()
+{
+    // Ports initialization
+    DDRL = 0x00;
+    DDRF = 0x01;
+    DDRH = 0x03;
+
+    // Serial communication initialization
+    USART0Init();
+    USART1Init();
+
+    if (PINL & (1 << PINL7))
+        MasterModule();
+    else
+        SlaveModule();
+}
+
+void MasterModule(void)
+{
+    char c, x, s, a1, a0;
+    char str[4];
+
+    PORTF = 1;
+    fprintf(&usart0_str, "<hash> *** MASTER *** \n\n");
+
+    while (1)
+    {
+        fprintf(&usart0_str, "Digite um comando aqui:\n");
+
+        // Receiving command from user and transmiting to slave
+        fscanf(&usart0_str, " %c%c%c%c%c", &c, &x, &s, &a1, &a0);
+        fprintf(&usart1_str, "%c%c%c%c%c", c, x, s, a1, a0);
+        fprintf(&usart0_str, "\n");
+
+        // Receiving status from slave and printing in terminal
+        fscanf(&usart1_str, " %c%c%c", &str[0], &str[1], &str[2]);
+        if (str[0] == 'I')
+        {
+            fscanf(&usart1_str, "%c%c%c%c", &str[3], &str[4], &str[5], &str[6]);
+            fprintf(&usart0_str, "INVALID\n\n");
+        }
+        else
+        {
+            fprintf(&usart0_str, "ACK\n\n");
+        }
+    }
+}
+
+void SlaveModule(void)
+{
+    char c, x, s, a1, a0;
+    unsigned int signal, angle;
+
+    // Enables output for 2 LEDs on PORTR and 3 servos on PORTB
+    DDRH = 0x03;
+    DDRB = 0xE0;
+
+    // Initialize tempo on MODE 14 to modulate PWM signal and set servos to angle 0°
+    initTempo();
+    resetServo();
+
+    // Identify SLAVE operation on LED and TERMINAL
+    PORTF = 0;
+    fprintf(&usart0_str, "<hash> *** SLAVE *** \n\n");
+
+    while (1)
+    {
+        // Listening to new inputs
+        fscanf(&usart1_str, " %c%c%c%c%c", &c, &x, &s, &a1, &a0);
+        fprintf(&usart0_str, "%c%c%c%c%c\n\n", c, x, s, a1, a0);
+
+        // If it is a valid protocol
+        if ((c == 'S') && (x >= '0' && x <= '2') && isSign(s) && isDigit(a1) && isDigit(a0) && (a1 < '9' || a0 == '0'))
+        {
+            fprintf(&usart1_str, "ACK");
+            angle = 10 * (a1 - '0') + (a0 - '0');
+            if (s == '-')
+                angle = -angle;
+
+            signal = round(100 * (270 + angle) / 9) - 1;
+
+            if (x == '0')
+                OCR1A = signal;
+            else if (x == '1')
+                OCR1B = signal;
+            else
+                OCR1CH = signal;
+        }
+        else if ((c == 'L') && (x == '0' || x == '1') && (s == 'O') && (a1 == a0) && (a0 == 'F' || a0 == 'N'))
+        {
+            fprintf(&usart1_str, "ACK");
+            PORTH = (a0 == 'F') ? (~(1 << (x - '0')) & PORTH) : ((1 << (x - '0')) | PORTH);
+        }
+        else
+            fprintf(&usart1_str, "INVALID");
+    }
+}
+
+void initTempo(void)
+{
+    TCCR1A = (1 << COM1A1) | (0 << COM1A0) | (1 << COM1B1) | (0 << COM1B0) | (1 << COM1C1) | (0 << COM1C0) | (1 << WGM11) | (0 << WGM10);
+    TCCR1B = (0 << ICNC1) | (0 << ICES1) | (1 << WGM13) | (1 << WGM12) | (0 << CS12) | (1 << CS11) | (0 << CS10);
+    TIMSK1 = (0 << ICIE1) | (0 << OCIE1C) | (0 << OCIE1B) | (0 << OCIE1A) | (0 << TOIE1);
+}
+
+void resetServo(void)
+{
+    OCR1AH = 11;
+    OCR1AL = 183;
+    OCR1BH = 11;
+    OCR1BL = 183;
+    OCR1CH = 11;
+    OCR1CL = 183;
+}
+
+/*
+ * USART METHODS
+ */
+
+void USART0Init(void)
+{
+    ICR1 = 39999;
+    UCSR0A = (0 << RXC0) | (0 << TXC0) | (0 << UDRE0) | (0 << FE0) | (0 << DOR0) | (0 << UPE0) | (0 << U2X0) | (0 << MPCM0);
+    UCSR0B = (0 << RXCIE0) | (0 << TXCIE0) | (0 << UDRIE0) | (1 << RXEN0) | (1 << TXEN0) | (0 << UCSZ02) | (0 << RXB80) | (0 << TXB80);
+    UCSR0C = (0 << UMSEL01) | (0 << UMSEL00) | (0 << UPM01) | (0 << UPM00) | (0 << USBS0) | (1 << UCSZ01) | (1 << UCSZ00) | (0 << UCPOL0);
+    UBRR0H = 0x00;
+    UBRR0L = 16; // for 57600bps baud rate
+}
+
+int USART0SendByte(char u8Data, FILE *stream)
+{
+    if (u8Data == '\n')
+        USART0SendByte('\r', stream);
+    // Wait for previous byte transmission
+    while (!(UCSR0A & (1 << UDRE0)))
+        ;
+    // Send byte
+    UDR0 = u8Data;
+    return 0;
+}
+
+int USART0ReceiveByte(FILE *stream)
+{
+    uint8_t u8Data;
+    // Wait byte receive
+    while (!(UCSR0A & (1 << RXC0)))
+        ;
+    u8Data = UDR0;
+    // Returns received byte
+    return u8Data;
+}
+
+void USART1Init(void)
+{
+    UCSR1A = (0 << RXC1) | (0 << TXC1) | (0 << UDRE1) | (0 << FE1) | (0 << DOR1) | (0 << UPE1) | (0 << U2X1) | (0 << MPCM1);
+    UCSR1B = (0 << RXCIE1) | (0 << TXCIE1) | (0 << UDRIE1) | (1 << RXEN1) | (1 << TXEN1) | (0 << UCSZ12) | (0 << RXB81) | (0 << TXB81);
+    UCSR1C = (0 << UMSEL11) | (0 << UMSEL10) | (0 << UPM11) | (0 << UPM10) | (0 << USBS1) | (1 << UCSZ11) | (1 << UCSZ10) | (0 << UCPOL1);
+    UBRR1H = 0x00;
+    UBRR1L = 16; // for 57600bps baud rate
+}
+
+int USART1SendByte(char u8Data, FILE *stream)
+{
+    if (u8Data == '\n')
+        USART1SendByte('\r', stream);
+    // Wait for previous byte transmission
+    while (!(UCSR1A & (1 << UDRE1)))
+        ;
+    // Send byte
+    UDR1 = u8Data;
+    return 0;
+}
+
+int USART1ReceiveByte(FILE *stream)
+{
+    uint8_t u8Data;
+    // Wait byte receive
+    while (!(UCSR1A & (1 << RXC1)))
+        ;
+    u8Data = UDR1;
+    // Returns received byte
+    return u8Data;
+}
